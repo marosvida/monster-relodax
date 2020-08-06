ba863f10c9ec8b4371680d88836b57d13ac60ab7
Maroš Vida <maros.vida@student.tuke.sk>
2020-08-06 10:26:10 +0200

    Updates

---
+++ precache-manifest.401f14bf018d2faf060ddba55ce8b6d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984bd5a4b0b121d5fc137f77d04cd919",
+    "url": "/marosvida/monster-relodax/index.html"
+  },
+  {
+    "revision": "8e861a959a87ae3e4392",
+    "url": "/marosvida/monster-relodax/static/css/main.e6b2f020.chunk.css"
+  },
+  {
+    "revision": "299f65059b0504546074",
+    "url": "/marosvida/monster-relodax/static/js/2.eca8dab0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marosvida/monster-relodax/static/js/2.eca8dab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e861a959a87ae3e4392",
+    "url": "/marosvida/monster-relodax/static/js/main.19adb37c.chunk.js"
+  },
+  {
+    "revision": "7f02a9e0e2b60ffa3b95",
+    "url": "/marosvida/monster-relodax/static/js/runtime-main.6db0d173.js"
+  }
+]);
